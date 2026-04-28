126e9bff31143cbc7ea13d0d6e0c22e6936a89cc
Nhlanhla2Nice <sibiyanhlanhla1105@gmail.com>
2026-04-28 22:57:20 +0200

    Done

--- Question2_SimpleATM/obj/Debug/net9.0/Question2_SimpleATM.AssemblyInfo.cs
+++ Question2_SimpleATM/obj/Debug/net9.0/Question2_SimpleATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Question2_SimpleATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ca1abd4d0c286ae7c6076a2889ff330f9e6330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8d29f33ff9e3e729ffb9a33661eea3ee1ac9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Question2_SimpleATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Question2_SimpleATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
